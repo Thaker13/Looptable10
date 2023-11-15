10988f8b62e3471d9d9510d401837341c1a3d151
Thaker13 <149239169+Thaker13@users.noreply.github.com>
2023-11-15 23:37:24 +0000

    Add files via upload

---
+++ Looptable10.java
@@ -0,0 +1,10 @@
+package looptable10;
+
+public class Looptable10 {
+    public static void main (String[] args){
+        for (int i =1; i<= 10; i++){
+            System.out.println("10 *" + i +"=" +10* i);
+
+        }
+    }
+}
